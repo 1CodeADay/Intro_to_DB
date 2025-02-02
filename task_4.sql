4b59d83460b7555573f8ce2127a987d617ddeac0
Rodrigue Danso <rodrig.danso@gmail.com>
2025-02-02 21:56:48 +0100

    Update 3

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
--- Query the full description of the 'Books' table
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
+-- Query the full description of the 'Books' table, including column types
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
